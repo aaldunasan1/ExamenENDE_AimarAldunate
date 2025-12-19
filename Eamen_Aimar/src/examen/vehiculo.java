c15fb34c678bbe04300b57828264cbacdbe7cff7
aaldunasan1 <aaldunasan1@educacion.navarra.es>
2025-12-19 09:00:05 +0100

    Constructor añadido

--- Eamen_Aimar/src/examen/vehiculo.java
+++ Eamen_Aimar/src/examen/vehiculo.java
@@ -2,4 +2,34 @@ package examen;
 
 public class vehiculo {
 
+	String matricula;
+	String marca;
+	String modelo;
+	double preciohora;
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public double getPreciohora() {
+		return preciohora;
+	}
+	public void setPreciohora(double preciohora) {
+		this.preciohora = preciohora;
+	}
+	
+	
 }
